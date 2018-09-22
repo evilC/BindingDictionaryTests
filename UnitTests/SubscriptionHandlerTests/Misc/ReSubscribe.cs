d712fa208f7984c761f85165fdda82ec66edf638
evilC <evilc@evilc.com>
2018-09-22 05:29:21 +0100

    Add Resubscribe test

---
+++ UnitTests/SubscriptionHandlerTests/Misc/ReSubscribe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BindingDictionaryTestOne;
+using NUnit.Framework;
+using UnitTests.Helpers;
+
+namespace UnitTests.SubscriptionHandlerTests.Misc
+{
+    [TestFixture]
+    class ReSubscribe
+    {
+        /// <summary>
+        /// Give I subscribe twice with the same SubscriberGuid to the same input of a SubscriptionHandler
+        /// When I unsibscribe once
+        /// Then the EmptyHandler fires
+        /// </summary>
+        [TestCase(TestName = "Subscribing twice to the same input and unsubscribing once unsubscribes")]
+        public void ResubscribeTest()
+        {
+            var subHelper = new SubscriptionHelper();
+            subHelper.SubHandler.Subscribe(subHelper.BuildSubReq(Lookups.SubReqs.Button1));
+            Assert.That(subHelper.DeviceEmptyResults.Count, Is.EqualTo(0));
+            subHelper.SubHandler.Subscribe(subHelper.BuildSubReq(Lookups.SubReqs.Button1));
+            Assert.That(subHelper.DeviceEmptyResults.Count, Is.EqualTo(0));
+            subHelper.SubHandler.Unsubscribe(subHelper.BuildSubReq(Lookups.SubReqs.Button1));
+            Assert.That(subHelper.DeviceEmptyResults.Count, Is.EqualTo(1));
+        }
+    }
+}
