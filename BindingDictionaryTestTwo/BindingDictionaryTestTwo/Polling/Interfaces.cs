f9ffab8723f9a9c33093ca792da81bf6338aa84a
evilC <evilc@evilc.com>
2018-09-25 17:47:37 +0100

    More comments

--- BindingDictionaryTestTwo/BindingDictionaryTestTwo/Polling/Interfaces.cs
+++ BindingDictionaryTestTwo/BindingDictionaryTestTwo/Polling/Interfaces.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace BindingDictionaryTestTwo.Polling
 {
+    /// <summary>
+    /// Interface for handling Axis value conversion, POV physical to logical values etc
+    /// </summary>
     public interface IUpdateProcessor
     {
+        // Converts an update for one physical input into one or more updates
+        // Eg POV changing angle from 0 to 90 deg would cause two updates - North release and East press
+        // In most other cases, the array will be of 1 length
         BindingUpdate[] Process(BindingUpdate update);
     }
 }
